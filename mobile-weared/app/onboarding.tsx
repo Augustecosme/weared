acf6339f4b08cabf58ce999474f7db6acb455ffb
Augustecosme <cooviquenum8@gmail.com>
2025-11-23 23:25:49 +0100

    fix: home

--- mobile-weared/app/onboarding.tsx
+++ mobile-weared/app/onboarding.tsx
@@ -154,7 +154,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    paddingBottom: 60,
+    paddingBottom: 80,
     paddingHorizontal: 40,
     backgroundColor: '#FFFFFF',
   },
